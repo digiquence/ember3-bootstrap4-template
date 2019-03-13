4ef6ae517f522f56919f72d18b9773feeb57341c
Raul Beltran <rb@digiquence.com>
2019-03-13 13:58:46 -0600

    production env for github pages

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.baseURL = '/ember3-bootstrap4-template';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
